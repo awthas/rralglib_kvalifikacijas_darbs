536214c4a1bebedb6cc0aaeeeeb9ad77934ca7e8
Awthas <immurka@gmail.com>
2025-12-28 10:19:32 +0200

    Added the remaining test cases for rralglib.py

--- python/rralglib_unit_testing.py
+++ python/rralglib_unit_testing.py
@@ -1,15 +1,15 @@
 import numpy as np
 import unittest
-import rr_algorithms
+import rralglib
 
-### Prototype unit tests for the Python rralglib module
+### Unit tests for the Python rralglib module
 
 class TestSRMAC(unittest.TestCase):
 
     def test_empty(self):
         signal = []
 
-        rr, peaks = rr_algorithms.srmac_inline(data=signal, fs=1)
+        rr, peaks = rralglib.srmac(data=signal, fs=1)
 
         self.assertEqual(rr, 0)
         self.assertEqual(peaks, [])
@@ -17,7 +17,7 @@ class TestSRMAC(unittest.TestCase):
     def test_one(self):
         signal = 1
 
-        rr, peaks = rr_algorithms.srmac_inline(data=signal, fs=1)
+        rr, peaks = rralglib.srmac(data=signal, fs=1)
 
         self.assertEqual(rr, 0)
         self.assertEqual(peaks, [])
@@ -25,7 +25,7 @@ class TestSRMAC(unittest.TestCase):
     def test_zeroes(self):
         signal = np.zeros(100)
 
-        rr, peaks = rr_algorithms.srmac_inline(data=signal, fs=1)
+        rr, peaks = rralglib.srmac(data=signal, fs=1)
 
         self.assertEqual(rr, 0)
         self.assertEqual(peaks, [])
@@ -34,7 +34,7 @@ class TestSRMAC(unittest.TestCase):
         fs = 64
         signal = np.sin(np.linspace(0,4*np.pi,fs*4))
 
-        rr, peaks = rr_algorithms.srmac_inline(data=signal, fs=fs, args=[-1,-1,-1,0,0.1,0])
+        rr, peaks = rralglib.srmac(data=signal, fs=fs, args=[-1,-1,-1,0,0.1,0])
 
         self.assertGreater(rr, 0)
         self.assertGreater(len(peaks), 0)
@@ -44,7 +44,7 @@ class TestTERMA(unittest.TestCase):
     def test_empty(self):
         signal = []
 
-        rr, peaks = rr_algorithms.terma_corrected(data=signal, fs=1)
+        rr, peaks = rralglib.terma(data=signal, fs=1)
 
         self.assertEqual(rr, 0)
         self.assertEqual(peaks, [])
@@ -52,7 +52,7 @@ class TestTERMA(unittest.TestCase):
     def test_one(self):
         signal = 1
 
-        rr, peaks = rr_algorithms.terma_corrected(data=signal, fs=1)
+        rr, peaks = rralglib.terma(data=signal, fs=1)
 
         self.assertEqual(rr, 0)
         self.assertEqual(peaks, [])
@@ -60,7 +60,7 @@ class TestTERMA(unittest.TestCase):
     def test_zeroes(self):
         signal = np.zeros(100)
 
-        rr, peaks = rr_algorithms.terma_corrected(data=signal, fs=1)
+        rr, peaks = rralglib.terma(data=signal, fs=1)
 
         self.assertEqual(rr, 0)
         self.assertEqual(peaks, [])
@@ -69,7 +69,7 @@ class TestTERMA(unittest.TestCase):
         fs = 64
         signal = np.sin(np.linspace(0,4*np.pi,fs*4))
 
-        rr, peaks = rr_algorithms.terma_corrected(data=signal, fs=fs, args=[0.2,0.6,-1,0.1,0])
+        rr, peaks = rralglib.terma(data=signal, fs=fs, args=[0.2,0.6,-1,0.1,0])
 
         self.assertGreater(rr, 0)
         self.assertGreater(len(peaks), 0)
@@ -79,7 +79,7 @@ class TestFindPeaks(unittest.TestCase):
     def test_empty(self):
         signal = []
 
-        rr, peaks = rr_algorithms.find_peaks_fast(data=signal, fs=1)
+        rr, peaks = rralglib.find_peaks(data=signal, fs=1)
 
         self.assertEqual(rr, 0)
         self.assertEqual(peaks, [])
@@ -87,7 +87,7 @@ class TestFindPeaks(unittest.TestCase):
     def test_one(self):
         signal = 1
 
-        rr, peaks = rr_algorithms.find_peaks_fast(data=signal, fs=1)
+        rr, peaks = rralglib.find_peaks(data=signal, fs=1)
 
         self.assertEqual(rr, 0)
         self.assertEqual(peaks, [])
@@ -95,7 +95,7 @@ class TestFindPeaks(unittest.TestCase):
     def test_zeroes(self):
         signal = np.zeros(100)
 
-        rr, peaks = rr_algorithms.find_peaks_fast(data=signal, fs=1)
+        rr, peaks = rralglib.find_peaks(data=signal, fs=1)
 
         self.assertEqual(rr, 0)
         self.assertEqual(peaks, [])
@@ -104,17 +104,17 @@ class TestFindPeaks(unittest.TestCase):
         fs = 64
         signal = np.sin(np.linspace(0,4*np.pi,fs*4))
 
-        rr, peaks = rr_algorithms.find_peaks_fast(data=signal, fs=fs, args=[-1,-1,-1,-1,-1,-1])
+        rr, peaks = rralglib.find_peaks(data=signal, fs=fs, args=[-1,-1,-1,-1,-1,-1])
 
         self.assertGreater(rr, 0)
         self.assertGreater(len(peaks), 0)
 
-class TestCWTPeaks(unittest.TestCase):
+class TestCWT(unittest.TestCase):
 
     def test_empty(self):
         signal = []
 
-        rr, peaks = rr_algorithms.cwt_peaks_oa(data=signal, fs=1, threshold=0.0)
+        rr, peaks = rralglib.cwt_peaks(data=signal, fs=1, threshold=0.0)
 
         self.assertEqual(rr, 0)
         self.assertEqual(peaks, [])
@@ -122,7 +122,7 @@ class TestCWTPeaks(unittest.TestCase):
     def test_one(self):
         signal = 1
 
-        rr, peaks = rr_algorithms.cwt_peaks_oa(data=signal, fs=1, threshold=0.0)
+        rr, peaks = rralglib.cwt_peaks(data=signal, fs=1, threshold=0.0)
 
         self.assertEqual(rr, 0)
         self.assertEqual(peaks, [])
@@ -130,7 +130,7 @@ class TestCWTPeaks(unittest.TestCase):
     def test_zeroes(self):
         signal = np.zeros(100)
 
-        rr, peaks = rr_algorithms.cwt_peaks_oa(data=signal, fs=1, threshold=0.0)
+        rr, peaks = rralglib.cwt_peaks(data=signal, fs=1, threshold=0.0)
 
         self.assertEqual(rr, 0)
         self.assertEqual(peaks, [])
@@ -139,7 +139,42 @@ class TestCWTPeaks(unittest.TestCase):
         fs = 64
         signal = np.sin(np.linspace(0,4*np.pi,fs*4))
 
-        rr, peaks = rr_algorithms.cwt_peaks_oa(data=signal, fs=fs, resolution=5, threshold=0.0, width=0.1, margin=0, min_freq=0.5, max_freq=4)
+        rr, peaks = rralglib.cwt_peaks(data=signal, fs=fs, resolution=5, threshold=0.0, width=0.1, margin=0, min_freq=0.5, max_freq=4)
+
+        self.assertGreater(rr, 0)
+        self.assertGreater(len(peaks), 0)
+
+class TestCWTOA(unittest.TestCase):
+
+    def test_empty(self):
+        signal = []
+
+        rr, peaks = rralglib.cwt_peaks_oa(data=signal, fs=1, threshold=0.0)
+
+        self.assertEqual(rr, 0)
+        self.assertEqual(peaks, [])
+
+    def test_one(self):
+        signal = 1
+
+        rr, peaks = rralglib.cwt_peaks_oa(data=signal, fs=1, threshold=0.0)
+
+        self.assertEqual(rr, 0)
+        self.assertEqual(peaks, [])
+
+    def test_zeroes(self):
+        signal = np.zeros(100)
+
+        rr, peaks = rralglib.cwt_peaks_oa(data=signal, fs=1, threshold=0.0)
+
+        self.assertEqual(rr, 0)
+        self.assertEqual(peaks, [])
+
+    def test_sine(self):
+        fs = 64
+        signal = np.sin(np.linspace(0,4*np.pi,fs*4))
+
+        rr, peaks = rralglib.cwt_peaks_oa(data=signal, fs=fs, resolution=5, threshold=0.0, width=0.1, margin=0, min_freq=0.5, max_freq=4)
 
         self.assertGreater(rr, 0)
         self.assertGreater(len(peaks), 0)
@@ -149,7 +184,7 @@ class TestZeroCrossing(unittest.TestCase):
     def test_empty(self):
         signal = []
 
-        count, peaks = rr_algorithms.zero_crossing(signal, width=1, fs=0, rawdata=None, margin=0, th=0.0)
+        count, peaks = rralglib.zero_crossing(signal, width=1, fs=0, rawdata=None, margin=0, th=0.0)
 
         self.assertEqual(count, 0)
         self.assertEqual(peaks, [])
@@ -157,7 +192,7 @@ class TestZeroCrossing(unittest.TestCase):
     def test_one(self):
         signal = [1]
 
-        count, peaks = rr_algorithms.zero_crossing(signal, width=1, fs=0, rawdata=None, margin=0, th=0.0)
+        count, peaks = rralglib.zero_crossing(signal, width=1, fs=0, rawdata=None, margin=0, th=0.0)
 
         self.assertEqual(count, 0)
         self.assertEqual(peaks, [])
@@ -165,7 +200,7 @@ class TestZeroCrossing(unittest.TestCase):
     def test_zeroes(self):
         signal = np.zeros(100)
 
-        count, peaks = rr_algorithms.zero_crossing(signal, width=1, fs=0, rawdata=None, margin=0, th=0.0)
+        count, peaks = rralglib.zero_crossing(signal, width=1, fs=0, rawdata=None, margin=0, th=0.0)
 
         self.assertEqual(count, 0)
         self.assertEqual(peaks, [])
@@ -173,7 +208,7 @@ class TestZeroCrossing(unittest.TestCase):
     def test_alternating(self):
         signal = np.array([0,1,0,1,0,1,0,1,0])
 
-        count, peaks = rr_algorithms.zero_crossing(signal, width=1, fs=0, rawdata=None, margin=0, th=0.0)
+        count, peaks = rralglib.zero_crossing(signal, width=1, fs=0, rawdata=None, margin=0, th=0.0)
 
         self.assertEqual(count, 4)
         self.assertEqual(peaks, [1,3,5,7])
@@ -181,7 +216,7 @@ class TestZeroCrossing(unittest.TestCase):
     def test_alternating_threshold(self):
         signal = np.array([0,0.5,0,0.7,0,0.9,0,1.1,0])
 
-        count, peaks = rr_algorithms.zero_crossing(signal, width=1, fs=0, rawdata=None, margin=0, th=0.8)
+        count, peaks = rralglib.zero_crossing(signal, width=1, fs=0, rawdata=None, margin=0, th=0.8)
 
         self.assertEqual(count, 2)
         self.assertEqual(peaks, [5,7])
@@ -189,7 +224,7 @@ class TestZeroCrossing(unittest.TestCase):
     def test_sine(self):
         signal = np.sin(np.linspace(0,4*np.pi,256))
 
-        count, peaks = rr_algorithms.zero_crossing(signal, width=1, fs=0, rawdata=None, margin=0, th=0.0)
+        count, peaks = rralglib.zero_crossing(signal, width=1, fs=0, rawdata=None, margin=0, th=0.0)
 
         self.assertGreater(count, 0)
         self.assertGreater(len(peaks), 0)
@@ -198,20 +233,20 @@ class TestFFT(unittest.TestCase):
 
     def test_zeroes(self):
         signal = np.zeros(256)
-        signal = rr_algorithms.fft(signal)
+        signal = rralglib.fft(signal)
 
         self.assertListEqual(signal.tolist(), np.zeros(256).tolist())
 
     def test_sine_single(self):
         signal = np.sin(np.linspace(0,16*np.pi,256)) ### 1Hz sine wave at a sampling rate of 32Hz
-        signal = rr_algorithms.fft(signal) ### n * 32 / 256 = 1Hz, therefore peak is expected at index 8
+        signal = rralglib.fft(signal) ### n * 32 / 256 = 1Hz, therefore peak is expected at index 8
 
         self.assertGreater(signal[8], signal[7])
         self.assertGreater(signal[8], signal[9])
 
     def test_sine_composite(self):
         signal = np.sin(np.linspace(0,16*np.pi,256))+np.sin(np.linspace(0,64*np.pi,256)) ### 1Hz sine wave and 4Hz sine wave at a sampling rate of 32Hz
-        signal = rr_algorithms.fft(signal) ### n * 32 / 256 = 1Hz/4Hz, therefore peaks expected at index 8 and 32
+        signal = rralglib.fft(signal) ### n * 32 / 256 = 1Hz/4Hz, therefore peaks expected at index 8 and 32
 
         self.assertGreater(signal[8], signal[7])
         self.assertGreater(signal[8], signal[9])
@@ -222,15 +257,15 @@ class TestIFFT(unittest.TestCase):
 
     def test_zeroes(self):
         signal = np.zeros(256)
-        fftsignal = rr_algorithms.fft(signal)
-        fftsignal = rr_algorithms.ifft(fftsignal)
+        fftsignal = rralglib.fft(signal)
+        fftsignal = rralglib.ifft(fftsignal)
 
         self.assertListEqual(signal.tolist(), fftsignal.real.tolist())
 
     def test_sine(self):
         signal = np.sin(np.linspace(0,16*np.pi,256)) ### 1Hz sine wave at a sampling rate of 32Hz
-        fftsignal = rr_algorithms.fft(signal)
-        fftsignal = rr_algorithms.ifft(fftsignal).real
+        fftsignal = rralglib.fft(signal)
+        fftsignal = rralglib.ifft(fftsignal).real
 
         signal = signal.tolist()
         for i in range(len(signal)):
@@ -242,21 +277,267 @@ class TestIFFT(unittest.TestCase):
         
         self.assertListEqual(signal, fftsignal)
 
-# sosfilt
+class TestWavelet(unittest.TestCase): ### All assert values calculated using a calculator and the appropriate wavelet definitions
+    def test_gaus2_zero(self):
+        t = 0
 
-# pearson
+        wavelet = rralglib.wavelet(t, tau=0, scale=1, wavelet="gaus2")
 
-# rmse
+        self.assertEqual(1.0314, round(wavelet, 4))
 
-# sqi
+    def test_gaus2_one(self):
+        t = 1
 
-# local maxima
+        wavelet = rralglib.wavelet(t, tau=0, scale=1, wavelet="gaus2")
 
-# count_orig
+        self.assertEqual(-0.3794, round(wavelet, 4))
 
-# count_adv
+    def test_gaus2_pi(self):
+        t = np.pi
 
-# bit rev
+        wavelet = rralglib.wavelet(t, tau=0, scale=1, wavelet="gaus2")
+
+        self.assertEqual(-0.0010, round(wavelet, 4))
+
+    def test_mexh_zero(self):
+        t = 0
+
+        wavelet = rralglib.wavelet(t, tau=0, scale=1, wavelet="mexh")
+
+        self.assertEqual(0.8673, round(wavelet, 4))
+
+    def test_mexh_one(self):
+        t = 1
+
+        wavelet = rralglib.wavelet(t, tau=0, scale=1, wavelet="mexh")
+
+        self.assertEqual(0.0, round(wavelet, 4))
+
+    def test_mexh_pi(self):
+        t = np.pi
+
+        wavelet = rralglib.wavelet(t, tau=0, scale=1, wavelet="mexh")
+
+        self.assertEqual(-0.0553, round(wavelet, 4))
+
+class TestSosfilt(unittest.TestCase): 
+    def test_lowpass_sine(self): ### Sine wave at a frequency of 1Hz, lowpass filtered with a 3rd order Butterworth filter at 0.5Hz; the signal after the transient should be close to zero
+        fs = 4
+        signal = np.sin(np.linspace(0,256*np.pi,fs*64))
+        sos = [[0.03168934,0.06337869,0.03168934,1.,-0.41421356,0.],[1.,1.,0.,1.,-1.0448155,0.47759225]]
+
+        signal = rralglib.sos_filt(signal, sos)
+
+        self.assertListEqual(np.zeros(64).tolist(), np.round(signal[-65:-1], 5).tolist())
+
+class TestPearson(unittest.TestCase):
+
+    def test_twonumbers_equal(self):
+        x = 1
+        y = 1
+
+        r = rralglib.pearson(x, y)
+
+        self.assertAlmostEqual(1.0, r)
+
+    def test_twoarrays_equal(self):
+        x = np.arange(128)
+        y = np.arange(128)
+
+        r = rralglib.pearson(x, y)
+
+        self.assertAlmostEqual(1.0, r)
+
+    def test_twoarrays_opposite(self):
+        x = np.arange(128) - 64
+        y = np.copy(x) * -1
+
+        r = rralglib.pearson(x, y)
+
+        self.assertAlmostEqual(-1.0, r)
+
+class TestSQIFull(unittest.TestCase):
+    def test_onepeak(self):
+        pattern = np.array([1,2,3,4,5,4,3,2])
+        signal = np.zeros(128)
+
+        for i in range(0,len(signal),len(pattern)):
+            signal[i:i+len(pattern)] = pattern
+
+        peaks = np.array([20])
+
+        sqi = rralglib.sqi_full(peaks, signal)
+
+        self.assertAlmostEqual(0.0, sqi)
+
+    def test_fourpeaks(self):
+        pattern = np.array([1,2,3,4,5,4,3,2])
+        signal = np.zeros(128)
+
+        for i in range(0,len(signal),len(pattern)):
+            signal[i:i+len(pattern)] = pattern
+
+        peaks = np.array([20, 28, 36, 52])
+
+        sqi = rralglib.sqi_full(peaks, signal)
+
+        self.assertAlmostEqual(1.0, sqi)
+
+    def test_twopeaks_unequal(self):
+        pattern = np.array([1,2,3,4,5,4,3,2])
+        signal = np.zeros(128)
+
+        for i in range(0,len(signal),len(pattern)):
+            signal[i:i+len(pattern)] = pattern
+
+        peaks = np.array([24, 48])
+
+        signal[25] = 0
+        signal[23] = 0
+
+        sqi = rralglib.sqi_full(peaks, signal)
+
+        self.assertGreater(1.0, sqi)
+
+class TestSQILite(unittest.TestCase):
+    def test_onepeak(self):
+        pattern = np.array([1,2,3,4,5,4,3,2])
+        signal = np.zeros(128)
+
+        for i in range(0,len(signal),len(pattern)):
+            signal[i:i+len(pattern)] = pattern
+
+        peaks = np.array([20])
+
+        sqi = rralglib.sqi_lite(peaks, signal)
+
+        self.assertAlmostEqual(0.0, sqi)
+
+    def test_fourpeaks(self):
+        pattern = np.array([1,2,3,4,5,4,3,2])
+        signal = np.zeros(128)
+
+        for i in range(0,len(signal),len(pattern)):
+            signal[i:i+len(pattern)] = pattern
+
+        peaks = np.array([20, 28, 36, 52])
+
+        sqi = rralglib.sqi_lite(peaks, signal)
+
+        self.assertAlmostEqual(1.0, sqi)
+
+    def test_twopeaks_unequal(self):
+        pattern = np.array([1,2,3,4,5,4,3,2])
+        signal = np.zeros(128)
+
+        for i in range(0,len(signal),len(pattern)):
+            signal[i:i+len(pattern)] = pattern
+
+        peaks = np.array([24, 48])
+
+        signal[25] = 0
+        signal[23] = 0
+
+        sqi = rralglib.sqi_lite(peaks, signal)
+
+        self.assertGreater(1.0, sqi)
+
+class TestCountOrig(unittest.TestCase):
+    def test_empty(self):
+        signal = []
+
+        rr, peaks = rralglib.count_orig(data=signal, fs=1)
+
+        self.assertEqual(rr, 0)
+        self.assertEqual(peaks, [])
+
+    def test_one(self):
+        signal = 1
+
+        rr, peaks = rralglib.count_orig(data=signal, fs=1)
+
+        self.assertEqual(rr, 0)
+        self.assertEqual(peaks, [])
+
+    def test_zeroes(self):
+        signal = np.zeros(100)
+
+        rr, peaks = rralglib.count_orig(data=signal, fs=1)
+
+        self.assertEqual(rr, 0)
+        self.assertEqual(peaks, [])
+
+    def test_sine(self):
+        fs = 64
+        signal = np.sin(np.linspace(0,4*np.pi,fs*4))
+
+        rr, peaks = rralglib.count_orig(data=signal, fs=fs)
+
+        self.assertGreater(rr, 0)
+        self.assertGreater(len(peaks), 0)
+
+class TestCountAdv(unittest.TestCase):
+    def test_empty(self):
+        signal = []
+
+        rr, peaks = rralglib.count_adv(data=signal, fs=1)
+
+        self.assertEqual(rr, 0)
+        self.assertEqual(peaks, [])
+
+    def test_one(self):
+        signal = 1
+
+        rr, peaks = rralglib.count_adv(data=signal, fs=1)
+
+        self.assertEqual(rr, 0)
+        self.assertEqual(peaks, [])
+
+    def test_zeroes(self):
+        signal = np.zeros(100)
+
+        rr, peaks = rralglib.count_adv(data=signal, fs=1)
+
+        self.assertEqual(rr, 0)
+        self.assertEqual(peaks, [])
+
+    def test_sine(self):
+        fs = 64
+        signal = np.sin(np.linspace(0,4*np.pi,fs*4))
+
+        rr, peaks = rralglib.count_adv(data=signal, fs=fs)
+
+        self.assertGreater(rr, 0)
+        self.assertGreater(len(peaks), 0)
+
+class TestBitrev(unittest.TestCase): ### Examples given in https://en.wikipedia.org/wiki/Bit-reversal_permutation used as test cases
+    def test_zero(self):
+        x = np.array([0])
+
+        x = rralglib.bit_reverse_sort(x)
+
+        self.assertListEqual([0],x.tolist())
+
+    def test_range_two(self):
+        x = np.arange(2)
+
+        x = rralglib.bit_reverse_sort(x)
+
+        self.assertListEqual([0,1],x.tolist())
+
+    def test_range_four(self):
+        x = np.arange(4)
+
+        x = rralglib.bit_reverse_sort(x)
+
+        self.assertListEqual([0,2,1,3],x.tolist())
+
+    def test_range_sixteen(self):
+        x = np.arange(16)
+
+        x = rralglib.bit_reverse_sort(x)
+
+        self.assertListEqual([0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15],x.tolist())
 
 if __name__ == '__main__':
     unittest.main()
